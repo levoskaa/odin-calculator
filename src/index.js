d99bde060b1aa351d09c9af58d53c9e0ad427c7f
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-05-02 17:29:07 +0200

    feat: implement calculator functionality wired to the UI

--- src/index.js
+++ src/index.js
@@ -12,15 +12,24 @@ class DivideByZeroError extends Error {
   }
 }
 
-function isNumber(value) {
-  const isNumber = typeof value === "number";
-  return isNumber;
+class ButtonType {
+  static Number = new ButtonType();
+  static ArithmeticOperator = new ButtonType();
+  static EqualOperator = new ButtonType();
+}
+
+function isNumeric(value) {
+  if (typeof value !== "number" && typeof value !== "string") {
+    return false;
+  }
+  const isNumeric = value !== "" && !isNaN(value);
+  return isNumeric;
 }
 
 function validateNumberValues(...values) {
-  const containsNaN = values.some((value) => !isNumber(value));
+  const containsNaN = values.some((value) => typeof value !== "number");
   if (containsNaN) {
-    throw new InvalidArgumentError("Input contains non-numeric values");
+    throw new InvalidArgumentError("Input contains non-number values");
   }
 }
 
@@ -56,7 +65,7 @@ function operate(operator, operand1, operand2) {
     case "+":
       return add(operand1, operand2);
     case "-":
-      return subtract(operand1, operand1);
+      return subtract(operand1, operand2);
     case "*":
       return multiply(operand1, operand2);
     case "/":
@@ -66,33 +75,124 @@ function operate(operator, operand1, operand2) {
   }
 }
 
-function onButtonClick() {
-  const value = this.dataset.value;
-  const isNumber = !isNaN(value);
-  if (isNumber) {
-    onNumberClick(value);
-  }
-}
-
-// TODO: handle overflow in the display
-function onNumberClick(digit) {
-  if (displayValue === "0") {
+function onNumberInput(digit) {
+  if (displayShouldReset) {
     doDisplayValue(digit);
+    displayShouldReset = false;
   } else {
     doDisplayValue(displayValue + digit);
   }
+  lastButtonPressed = ButtonType.Number;
+}
+
+function onArithmeticOperatorInput(operator) {
+  if (hasErrored()) {
+    return;
+  }
+  if (lastButtonPressed === ButtonType.ArithmeticOperator) {
+    // If arithmetic operations are pressed one after the other,
+    // then just update the operator to the latest one
+    savedOperator = operator;
+    return;
+  }
+  if (savedOperator) {
+    // If there is an operation that was not finalized, then we calculate
+    // its result now to reuse it in later operations
+    const activeOperand = +displayValue;
+    const result = operate(savedOperator, savedOperand, activeOperand);
+    doDisplayValue(result);
+    savedOperand = result;
+  } else {
+    savedOperand = +displayValue;
+  }
+  savedOperator = operator;
+  displayShouldReset = true;
+  lastButtonPressed = ButtonType.ArithmeticOperator;
+}
+
+function onEqualOperatorInput() {
+  // Use non-strict equality check to check for both undefined and null
+  const cannotCalculate =
+    lastButtonPressed !== ButtonType.Number ||
+    savedOperand == null ||
+    savedOperator == null;
+  if (cannotCalculate) {
+    return;
+  }
+  const activeOperand = +displayValue;
+  let result;
+  try {
+    result = operate(savedOperator, savedOperand, activeOperand);
+  } catch (e) {
+    result = "Err";
+  }
+  doDisplayValue(result);
+  displayShouldReset = true;
+  savedOperator = null;
+  savedOperand = null;
+  lastButtonPressed = ButtonType.EqualOperator;
 }
 
 function doDisplayValue(value) {
+  if (isNumeric(value)) {
+    value = trimNumber(value, CHARACTER_LIMIT);
+  }
   displayValue = value;
   display.textContent = displayValue;
 }
 
-let displayValue = "0";
-let operand2;
-let operator;
+function trimNumber(number, characterLimit) {
+  // Use parseFloat to remove leading zeros
+  const trimmed = parseFloat(number);
+  const length = trimmed.toString().length;
+  if (length <= characterLimit) {
+    return trimmed.toString();
+  }
+  return trimmed.toExponential(5);
+}
 
-const buttons = document.querySelectorAll(".calculator__button");
+function clear() {
+  displayValue = "0";
+  doDisplayValue(displayValue);
+  savedOperand = null;
+  savedOperator = null;
+  displayShouldReset = true;
+  lastButtonPressed = null;
+}
+
+function hasErrored() {
+  return displayValue === "Err";
+}
+
+const ARITHMETIC_OPERATIONS = ["+", "-", "*", "/"];
+const CHARACTER_LIMIT = 14;
+
+let displayValue = "0";
+let savedOperand;
+let savedOperator;
+let displayShouldReset = true;
+let lastButtonPressed;
+
+const buttons = Array.from(document.querySelectorAll(".calculator__button"));
+const numberButtons = buttons.filter((button) =>
+  isNumeric(button.dataset.value)
+);
+const arithmeticButtons = buttons.filter((button) =>
+  ARITHMETIC_OPERATIONS.includes(button.dataset.value)
+);
+const equalButton = buttons.find((button) => button.dataset.value === "=");
+const clearButton = buttons.find((button) => button.dataset.value === "C");
 const display = document.querySelector(".calculator__display");
 
-buttons.forEach((button) => button.addEventListener("click", onButtonClick));
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener("click", (e) =>
+    onNumberInput(e.currentTarget.dataset.value)
+  );
+});
+arithmeticButtons.forEach((arithmeticButton) => {
+  arithmeticButton.addEventListener("click", (e) =>
+    onArithmeticOperatorInput(e.currentTarget.dataset.value)
+  );
+});
+equalButton.addEventListener("click", onEqualOperatorInput);
+clearButton.addEventListener("click", clear);
